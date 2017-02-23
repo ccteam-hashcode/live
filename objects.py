ce761febc63751da3e37601826c1fbf6f2bf7ce5
Jakub Kačur <jakub.kacur@smartbase.sk>
2017-02-23 19:05:43 +0100

    system added to objects, basic methods

--- objects.py
+++ objects.py
@@ -1,16 +1,30 @@
+class System:
+    def __init__(self):
+        self.cache_servers = []
+        self.endpoints = []
+        self.requests = []
+
+
 class Video:
     def __init__(self, size):
         self.size = size
 
 
 class CacheServer:
-    def __init__(self):
+    def __init__(self, capacity):
         self.videos = []
-        self.capacity = 0
+        self.capacity = capacity
+
+    def add_video(self, size):
+        v = Video(size)
+        self.videos.append(v)
+
+    def get_video(self, n):
+        return self.videos[n]
 
 
 class Endpoint:
-    def __init__(self):
+    def __init__(self, latency_center):
         self.latency_cache = []
         self.latency_center = 0
 
